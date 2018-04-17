3811c27229c29e390e102f5d133b52a4b5b4088b
“Espen <admin@altonline.no>
2018-04-17 22:10:41 +0200

    improved iframe

--- resources/views/layouts/form/show.blade.php
+++ resources/views/layouts/form/show.blade.php
@@ -1,10 +1,3 @@
-@php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-
-@endphp
-
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
