2791b1f6ac4eedab68bc8b0aea609f22bb22c65e
“Espen <admin@altonline.no>
2018-05-13 10:45:31 +0200

    register text changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
                 <div class="py-1"><svg class="fill-current h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                 <div>
                     <p class="font-bold">Produktet er i en beta-versjon og under test!</p>
-                    <p class="text-sm">Produktet er p.t under test og det kan være enkelte ustabiliteter med tjenesten. Brukes på eget ansvar.</p>
+                    <p class="text-sm">Produktet er p.t under test og det kan være enkelte ustabiliteter med tjenesten.</p>
                 </div>
             </div>
         </div>
@@ -99,7 +99,7 @@
                 <div class="py-1"><svg class="fill-current h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                 <div>
                     <p class="font-bold">Produktet er i en beta-versjon og under test!</p>
-                    <p class="text-sm">Produktet er p.t under test og det kan være enkelte ustabiliteter med tjenesten. Brukes på eget ansvar.</p>
+                    <p class="text-sm">Produktet er p.t under test og det kan være enkelte ustabiliteter med tjenesten.</p>
                 </div>
             </div>
         </div>
